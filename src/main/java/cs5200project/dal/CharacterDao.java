4679dd4da04d6d7cd9fba17aaa1d3d3821d12af3
Ziqun Liu <ziqun.liu@yahoo.com>
2025-03-24 17:34:35 -0700

    Added inline comments to ChracterDao.java

--- src/main/java/cs5200project/dal/CharacterDao.java
+++ src/main/java/cs5200project/dal/CharacterDao.java
@@ -1,16 +1,29 @@
 package cs5200project.dal;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 import cs5200project.model.Character;
-import java.sql.*;
-import java.util.*;
 
 public class CharacterDao {
     protected Connection connection;
 
+	// Dao classes should not be instantiated.
+	// Pass Connection object as parameter in each method
+	// Each method should be static
     public CharacterDao(Connection connection) {
         this.connection = connection;
     }
 
+//	public static Character create (Connection cxn, int playerID, String
+//	 firstName, String lastName, int raceID, Date creationTime, boolean
+//	 isNewPlayer, int currentJobID) {
+//		
+//	}
+
+	// Add Connection cxn as parameter
     public Character getCharacterById(int id) throws SQLException {
         String query = "SELECT * FROM Character WHERE characterID = ?";
         try (PreparedStatement stmt = connection.prepareStatement(query)) {
