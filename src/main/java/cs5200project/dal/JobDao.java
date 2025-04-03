0fc8fe0ba58906e1d8cd58fb2836bef6d8df2295
Ziqun Liu <ziqun.liu@yahoo.com>
2025-04-03 00:15:12 -0700

    Updated 4 files

--- src/main/java/cs5200project/dal/JobDao.java
+++ src/main/java/cs5200project/dal/JobDao.java
@@ -5,8 +5,6 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.List;
 
 import cs5200project.model.Job;
 
@@ -29,17 +27,18 @@ public class JobDao {
         }
     }
 
-    public static Job getJobById(Connection cxn, Job job) throws SQLException {
+	public static Job getJobById(Connection cxn, int jobId)
+			throws SQLException {
         final String selectJob = """
             SELECT * FROM Job 
               WHERE jobID = ?;
         """;
         try (PreparedStatement selectStmt = cxn.prepareStatement(selectJob)) {
-            selectStmt.setInt(1, job.getJobID());
+			selectStmt.setInt(1, jobId);
             try (ResultSet results = selectStmt.executeQuery()) {
                 if (results.next()) {
                     String jobName = results.getString("jobName");
-                    return new Job(job.getJobID(), jobName);
+					return new Job(jobId, jobName);
                 } else {
                     return null;
                 }
